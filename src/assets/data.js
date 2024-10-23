731dfc14f538b413e2a1b8a1ef6f2dc821981fc7
Luciano Tito 1801 <lucianotitocedron@gmail.com>
2024-10-23 19:24:31 -0300

    fix images

--- src/assets/data.js
+++ src/assets/data.js
@@ -3,61 +3,60 @@ export const data = [
     id: 1,
     product: "Callera Cobra Pro",
     images: [
-      { id: 1, imgUrl: ".assets/images/calleraCobraPro1.jpeg " },
-      { id: 2, imgUrl: "src/assets/images/calleracobrapro2.jpeg" },
-      { id: 3, imgUrl: "src/assets/images/calleracobrapro3.jpeg" },
-      { id: 4, imgUrl: "src/assets/images/calleracobrapro4.jpeg" },
+      { id: 1, imgUrl: "/images/calleraCobraPro1.jpeg" },
+      { id: 2, imgUrl: "/images/calleracobrapro2.jpeg" },
+      { id: 3, imgUrl: "/images/calleracobrapro3.jpeg" },
+      { id: 4, imgUrl: "/images/calleracobrapro4.jpeg" },
     ],
   },
   {
     id: 2,
     product: "Callera Pro",
     images: [
-      { id: 5, imgUrl: "src/assets/images/callerapro1.jpeg" },
-      { id: 6, imgUrl: "src/assets/images/callerapro2.jpeg" },
-      { id: 7, imgUrl: "src/assets/images/callerapro3.jpeg" },
+      { id: 5, imgUrl: "/images/callerapro1.jpeg" },
+      { id: 6, imgUrl: "/images/callerapro2.jpeg" },
+      { id: 7, imgUrl: "/images/callerapro3.jpeg" },
     ],
   },
   {
     id: 3,
     product: "Cinta de Tela",
     images: [
-      { id: 8, imgUrl: "src/assets/images/cintadetela1.jpeg" },
-      { id: 9, imgUrl: "src/assets/images/cintadetela2.jpeg" },
+      { id: 8, imgUrl: "/images/cintadetela1.jpeg" },
+      { id: 9, imgUrl: "/images/cintadetela2.jpeg" },
     ],
   },
   {
     id: 4,
     product: "Cinto",
     images: [
-      { id: 10, imgUrl: "src/assets/images/cinto1.jpeg" },
-      { id: 11, imgUrl: "src/assets/images/cinto2.jpeg" },
+      { id: 10, imgUrl: "/images/cinto1.jpeg" },
+      { id: 11, imgUrl: "/images/cinto2.jpeg" },
     ],
   },
   {
     id: 5,
     product: "Muñequera de Tela",
     images: [
-      { id: 12, imgUrl: "src/assets/images/muñequera_tela1.jpeg" },
-      { id: 13, imgUrl: "src/assets/images/muñequeratela2.jpeg" },
+      { id: 12, imgUrl: "/images/muñequera_tela1.jpeg" },
+      { id: 13, imgUrl: "/images/muñequeratela2.jpeg" },
     ],
   },
   {
     id: 6,
     product: "Rodillera",
     images: [
-      { id: 14, imgUrl: "src/assets/images/rodillera.webp" },
-      { id: 15, imgUrl: "src/assets/images/rodillera_talle.webp" },
+      { id: 14, imgUrl: "/images/rodillera.webp" },
+      { id: 15, imgUrl: "/images/rodillera_talle.webp" },
     ],
   },
-
   {
     id: 7,
-    product: "Muñequera elastica",
+    product: "Muñequera elástica",
     images: [
-      { id: 16, imgUrl: "src/assets/images/muñequera_elastica1.webp" },
-      { id: 17, imgUrl: "src/assets/images/muñequera_elastica2.webp" },
-      { id: 18, imgUrl: "src/assets/images/muñequera_elastica3.webp" },
+      { id: 16, imgUrl: "/images/muñequera_elastica1.webp" },
+      { id: 17, imgUrl: "/images/muñequera_elastica2.webp" },
+      { id: 18, imgUrl: "/images/muñequera_elastica3.webp" },
     ],
   },
 ];
